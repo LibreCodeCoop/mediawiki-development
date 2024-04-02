ce68c38e9f0660fd4bfe94e8e1e066198e4e974c
Vitor Mattos <vitor@php.rio>
2024-04-02 12:20:49 -0300

    Create link if not exists
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- .docker/mediawiki/scripts/init.php
+++ .docker/mediawiki/scripts/init.php
@@ -3,9 +3,12 @@
 chdir('/var/www/mediawiki');
 
 foreach (glob('../src/mediawiki/*') as $origin) {
-	$destination = str_replace('/src/', '/', $origin);
+	$destination = str_replace('../src/mediawiki/', '', $origin);
 	if (is_file($destination) || is_dir($destination)) {
 		exec('rm -rf ' . $destination);
+		exec('ln -s ' . $origin . ' ' . $destination);
+	}
+	if (!is_link($destination)) {
 		exec('ln -s ' . $origin . ' ' . $destination);
 	}
 }
