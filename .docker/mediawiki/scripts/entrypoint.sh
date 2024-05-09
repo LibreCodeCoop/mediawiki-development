d0df247d0821c0250aa40062094ab659d0b90583
Lívia Gouvêa <livia@mail.mayfirst.org>
2024-05-08 21:33:48 -0300

    Altera owner para usuário padrão do Apache
    Signed-off-by: Lívia Gouvêa <livia@mail.mayfirst.org>

--- .docker/mediawiki/scripts/entrypoint.sh
+++ .docker/mediawiki/scripts/entrypoint.sh
@@ -9,6 +9,8 @@ php /var/www/scripts/wait-for-db.php
 
 php /var/www/scripts/init.php
 
+chown -R www-data:www-data /var/www/mediawiki
+
 sed -i -e "s/\/var\/www\/html/\/var\/www\/mediawiki/" /etc/apache2/sites-available/000-default.conf
 
 apache2-foreground
